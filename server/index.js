17485e594eb988ccb0b7fc033f167e080f60fcd3
devkrishna18 <krishyadav18.dev@gmail.com>
2026-01-10 22:56:32 +0530

    Refactor server/index.js for clarity and updates

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const { connectDB } = require('./config/db');
 
-// IMPORT from socket.js (Ye sabse important line hai)
+// IMPORT from socket.js
 const { app, server } = require("./socket.js");
 
 const userRoutes = require('./routes/userRoutes');
@@ -12,19 +12,20 @@ const msgRoutes = require("./routes/msgRoutes");
 
 dotenv.config();
 
-
-
-// 1. Middlewares (Routes se pehle aane chahiye)
-app.use(express.json({ limit: '10mb' })); // Image upload ke liye size badhaya
+// 1. Middlewares
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// CORS Setup (Frontend 5173 ke liye allow karein)
+// CORS Setup
 app.use(cors({
-    origin:["http://localhost:5173",
-        "https://chit-chat-gamma-six.vercel.app"
+    origin: [
+        "http://localhost:5173",                     
+        "https://chit-chat-eight-delta.vercel.app",  
+        "https://chit-chat-gamma-six.vercel.app",    
+        process.env.CLIENT_URL                       
     ],
-    credentials: true // Cookies bhejne ke liye zaruri hai
+    credentials: true
 }));
 
 // 2. Database Connect
@@ -37,10 +38,10 @@ app.use("/api/messages", msgRoutes);
 // Health Check Route
 app.use("/api/status", (req, res) => res.send("Server is live and Socket is ready"));
 
-// 4. Server Listen (Server use karein, app nahi)
-if(process.env.NODE_ENV !== 'production'){
-    const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})};
+});
+
 module.exports = app;
